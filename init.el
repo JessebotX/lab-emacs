8e700120f3525ff147ab245c5428d0d2ba831300
Jesse <jessebot.git@gmail.com>
2025-05-01 10:51:12 -0700

    feat: support org mode

--- init.el
+++ init.el
@@ -35,6 +35,7 @@
     (json :size 4 :use-tabs nil)
     (lisp :size 8 :use-tabs nil)
     (md   :size 2 :use-tabs nil)
+    (org  :size 8 :use-tabs nil)
     (ts   :size 4 :use-tabs nil)
     (xml  :size 4 :use-tabs nil)
     (yaml :size 2 :use-tabs nil))
@@ -231,6 +232,13 @@ folder, otherwise delete a word."
 (add-hook 'prog-mode-hook #'whitespace-mode)
 
 ;; FONT
+(defun my/set-default-font-family (family)
+  "Set the default face family to FAMILY, where FAMILY is a string
+specifying the font family name. If font family does not exist, nothing
+will occur."
+  (interactive "sFont family: ")
+  (set-face-attribute 'default nil :family family))
+
 (set-face-attribute 'default nil :family "Maple Mono" :height 120)
 (set-fontset-font
  t
@@ -314,6 +322,11 @@ folder, otherwise delete a word."
   :ensure t
   :commands (olivetti-mode))
 
+;;; WRITEROOM
+(use-package writeroom-mode
+  :ensure t
+  :commands (writeroom-mode global-writeroom-mode))
+
 ;;; TREE-SITTER LANGUAGE SUPPORT
 (use-package tree-sitter
   :ensure t
@@ -329,6 +342,12 @@ folder, otherwise delete a word."
   :ensure (:host github :repo "orzechowskid/tsi.el")
   :commands (tsi-typescript-mode))
 
+;;; MAGIT
+(use-package magit
+  :ensure t
+  :commands (magit-status magit))
+
+;;; MEOW KEYMAP
 (use-package meow
   :ensure t
   :custom
@@ -502,6 +521,120 @@ folder, otherwise delete a word."
   (markdown-max-image-size '(512 . 512))
   :config
   (add-hook 'markdown-mode-hook #'my/hook--markdown-mode))
+
+;;;; ORG
+(use-package org
+  :ensure nil
+  :preface
+  (defun my/hook--org-mode ()
+    "Settings for `org-mode-hook'"
+    (let ((size (my/lang-indent-size 'org))
+          (use-tabs (my/lang-indent-use-tabs 'org)))
+      (my/config-indent size use-tabs))
+    (setq paragraph-start "\\|[    ]*$")
+    (setq paragraph-separate "[     ]*$")
+    (visual-line-mode 1)
+    (display-line-numbers-mode -1))
+  :hook (org-mode-hook . my/hook--org-mode)
+  :custom
+  (org-hide-leading-stars t)
+  (org-id-link-to-org-use-id 'use-existing)
+  (org-ellipsis " +")
+  (org-auto-align-tags nil)
+  (org-src-preserve-indentation t)
+  (org-link-frame-setup '((vm . vm-visit-folder-other-frame)
+                          (vm-imap . vm-visit-imap-folder-other-frame)
+                          (gnus . org-gnus-no-new-news)
+                          (file . find-file)
+                          (wl . wl-other-frame)))
+  (org-link-file-path-type 'relative)
+  (org-hide-emphasis-markers t)
+  (org-startup-folded 'showall)
+  (org-todo-keywords '((sequence
+                        "TODO(t)"
+                        "WIP(w)"
+                        "HOLD(h)"
+                        "EDIT(e)"
+                        "REDO(r)"
+                        "APPOINTMENT(a)"
+                        "|" ; separate active and inactive states
+                        "ARCHIVED(s)"
+                        "DONE(d)"
+                        "CANCELLED(w)")))
+  (org-agenda-span 5)
+  (org-agenda-start-day "+0d")
+  (org-agenda-skip-timestamp-if-done t)
+  (org-agenda-skip-deadline-if-done t)
+  (org-agenda-skip-scheduled-if-done t)
+  (org-agenda-skip-scheduled-if-deadline-is-shown t)
+  (org-agenda-skip-timestamp-if-deadline-is-shown t)
+  (org-startup-with-inline-images t)
+  (org-tags-column 0)
+  :config
+  (add-to-list 'org-export-backends 'md)
+
+  ;; let j,k move around `org-agenda-mode'
+  (with-eval-after-load 'evil
+    (evil-define-key '(normal emacs) org-agenda-mode-map
+      (kbd "j") 'org-agenda-next-item
+      (kbd "k") 'org-agenda-previous-item
+      (kbd "N") 'org-agenda-goto-date
+      (kbd "P") 'org-agenda-capture))
+
+  ;; Commands/functions/macros
+  (defun my/org-toggle-markup ()
+    "Toggle hiding/showing `org-mode' emphasis markers."
+    (interactive)
+    (if org-hide-emphasis-markers
+        (setopt org-hide-emphasis-markers nil)
+      (setopt org-hide-emphasis-markers t))
+    (org-mode-restart))
+
+  ;; Binds
+  (let ((map org-mode-map))
+    (keymap-set map "C-'" nil) ; remove org agenda cycle list
+    (keymap-set map "C-," nil) ; remove org agenda cycle list
+    (keymap-set map "C-c m i" 'org-id-store-link)
+    (keymap-set map "C-c m l" 'org-store-link)
+    (keymap-set map "C-c C-x RET" #'my/org-toggle-markup)))
+
+(use-package org-modern
+  :ensure t
+  :hook ((org-mode-hook . global-org-modern-mode)
+         (org-agenda-mode-hook . global-org-modern-mode))
+  :custom
+  (org-modern-keyword "» ")
+  (org-modern-star '("◉" "●" "○" "◈" "◇"))
+  (org-modern-block-fringe nil)
+  (org-modern-hide-stars t)
+  :config
+  (defun my/org-modern-hide-star-headings ()
+    "Remove all stars from a heading, increase heading sizes and disable `org-indent-mode'."
+    (interactive)
+    (setopt org-modern-hide-stars t)
+    (setopt org-hide-leading-stars t)
+    (org-mode-restart)
+    (org-indent-mode -1))
+
+  (defun my/org-modern-star-headings ()
+    "Enable stars and `org-indent-mode'."
+    (interactive)
+    (setopt org-modern-hide-stars nil)
+    (setopt org-hide-leading-stars nil)
+    (org-mode-restart)
+    (org-indent-mode 1)))
+
+(use-package org-wc
+  :ensure t
+  :commands org-wc-display)
+
+(use-package org-appear
+  :ensure t
+  :hook (org-mode-hook . org-appear-mode))
+
+(use-package ox-epub
+  :ensure t
+  :commands (org-export-dispatch))
 
 ;;;; TYPESCRIPT
 (use-package typescript-mode

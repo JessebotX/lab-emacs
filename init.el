f5ef160558a4793911a7718c21f98d28c73d6699
Jesse <jessebot.git@gmail.com>
2026-01-20 17:55:24 -0800

    add my/open-terminal

--- init.el
+++ init.el
@@ -785,6 +785,22 @@ buffer/file contents."
                      (diff-buffer-with-file (buffer-file-name buffer)))
                    "show diff between the buffer and its file"))
 
+(defun my/open-terminal ()
+  "Open the current dir in a new terminal window.
+
+URL `http://xahlee.info/emacs/emacs/emacs_open_in_terminal.html'
+Version: 2020-11-21 2022-08-04 2023-03-01 2023-06-26"
+  (interactive)
+  (cond
+   ((eq system-type 'windows-nt)
+    (shell-command (format "wt -d \"%s\"" default-directory)))
+   ((eq system-type 'darwin)
+    (shell-command (concat "open -a terminal " (shell-quote-argument (expand-file-name default-directory)))))
+   ((eq system-type 'gnu/linux)
+    (let ((process-connection-type nil)) (start-process "" nil "x-terminal-emulator" (concat "--working-directory=" default-directory))))
+   ((eq system-type 'berkeley-unix)
+    (let ((process-connection-type nil)) (start-process "" nil "x-terminal-emulator" (concat "--working-directory=" default-directory))))))
+
 (defun my/open-file ()
   "Open current buffer/file in external app.
 
